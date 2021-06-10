8520778d00953285217c99513876f21e5d9f259c
mardowiak <mardowiak@gmail.com>
2021-06-10 06:15:43 +0200

    Delete MemeListCreator.js

--- src/components/memes/MemesListCreator.js
+++
@@ -1,9 +0,0 @@
-import {useContext} from "react";
-import { MemesContext } from "../context/MemesContext";
-
-export default function MemesListCreator(){
-const memeData= useContext(MemesContext);
-return(<div>
-    <h1>{memeData[0].title}</h1>
-</div>)
-}
